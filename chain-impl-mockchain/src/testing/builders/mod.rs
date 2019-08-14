699ba7c74593f22ca2fed2342d004f1dde9caece
dkijania <dariusz.kijania@iohk.io>
2019-08-14 11:22:39 +0200

    Introduced proposal builder. fmt

--- chain-impl-mockchain/src/testing/builders/mod.rs
+++ chain-impl-mockchain/src/testing/builders/mod.rs
@@ -1,7 +1,9 @@
 pub mod tx_cert_builder;
 pub mod tx_builder;
 pub mod witness_builder;
+pub mod update_builder;
 
 pub use tx_cert_builder::*;
 pub use tx_builder::*;
 pub use witness_builder::*;
+pub use update_builder::*;
