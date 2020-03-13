555c73a8ce3d2889c840a186aaa779f316ba664b
Enzo Cioppettini <ecioppettini@atixlabs.com>
2020-03-13 16:54:31 -0300

    add backtrack module

--- btree/src/btreeindex/mod.rs
+++ btree/src/btreeindex/mod.rs
@@ -1,10 +1,11 @@
+mod backtrack;
 mod metadata;
 mod node;
 mod page_manager;
 mod pages;
 mod version_management;
 
-use version_management::transaction::{InsertTransaction, PageRefMut, ReadTransaction};
+use version_management::transaction::{PageRefMut, ReadTransaction, WriteTransaction};
 use version_management::*;
 
 use crate::mem_page::MemPage;
@@ -20,6 +21,7 @@ use std::borrow::Borrow;
 
 use crate::{Key, Value};
 
+use backtrack::{DeleteBacktrack, InsertBacktrack};
 use parking_lot::RwLock;
 use std::convert::{TryFrom, TryInto};
 use std::fs::{File, OpenOptions};
@@ -210,12 +212,11 @@ where
 
     fn insert<'a>(
         &self,
-        tx: &mut InsertTransaction<'a, 'a>,
+        tx: &mut WriteTransaction<'a, 'a>,
         key: K,
         value: Value,
     ) -> Result<(), BTreeStoreError> {
-        let mut backtrack = tx.backtrack();
-        backtrack.search_for(&key);
+        let mut backtrack = InsertBacktrack::new_search_for(tx, &key);
 
         let needs_recurse = {
             let leaf = backtrack.get_next()?.unwrap();
@@ -406,13 +407,11 @@ where
     fn delete_async<'a>(
         &'a self,
         key: &K,
-        tx: &mut InsertTransaction<'a, 'a>,
+        tx: &mut WriteTransaction<'a, 'a>,
     ) -> Result<(), BTreeStoreError> {
         let key_buffer_size: u32 = self.static_settings.key_buffer_size;
 
-        let mut backtrack = tx.delete_backtrack();
-
-        backtrack.search_for(key);
+        let mut backtrack = DeleteBacktrack::new_search_for(tx, key);
 
         // path loaded (cloned) in transaction
         let next_element = backtrack.get_next()?.unwrap();
